ef019c213ee4b1a11c835c78c7e5d5eaa04254a3
Amit Kumar Patra <amitpatra28061996@gmail.com>
2025-04-17 13:20:17 +0530

    Ptorobuf

---
+++ Common/ProtobufData.cs
@@ -0,0 +1,32 @@
+﻿using Google.Protobuf;
+using Tunetastic.Generated.Protos;
+
+namespace Tunetastic.Common;
+public class ProtobufData
+{
+    public static void SaveDataInBin(List<Song> songList)
+    {
+        SongList songsContainer = new SongList();
+        songsContainer.Songs.AddRange(songList);
+        using (FileStream output = File.Create(Constants.RootDirectoryPath + $"\\{DataFile.AllSongsMetaData}.bin"))
+        {
+            songsContainer.WriteTo(output);
+        }
+    }
+
+    public static SongList LoadDataFromBin(DataFile dataFile)
+    {
+        if (!File.Exists(Constants.RootDirectoryPath + $"\\{dataFile}.bin")) return new SongList();
+
+        using (FileStream input = File.OpenRead(Constants.RootDirectoryPath + $"\\{dataFile}.bin"))
+        {
+            return SongList.Parser.ParseFrom(input);
+        }
+    }
+
+}
+
+public enum DataFile
+{
+    AllSongsMetaData
+}
