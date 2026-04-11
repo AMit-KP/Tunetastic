f693841405a4b440017a1d0562b7cf520443457f
Amit Kumar Patra <amitpatra28061996@gmail.com>
2026-04-11 23:41:08 +0530

    minor fix

--- Views/PlaylistViews/RecentlyPlayed.xaml.cs
+++ Views/PlaylistViews/RecentlyPlayed.xaml.cs
@@ -964,6 +964,7 @@ public sealed partial class RecentlyPlayed : Page
 				if (container != null)
 				{
 					var song = container.Content as Song;
+					if (song == null) continue;
 					var relativeLiveTime = relativeTimeConverter.Convert(song.DateLastPlayed, null, null, null).ToString();
 					var textBlock = (DevWinUI.DependencyObjectExtensions.FindDescendant(container, "RelativeTime") as TextBlock);
 					textBlock?.Text = relativeLiveTime;
